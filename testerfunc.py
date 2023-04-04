d02767d967cf5135143e03865ff87cd293ae8e10
Benyla <122599452+Benyla@users.noreply.github.com>
2023-04-04 13:22:43 +0200

    Add files via upload
    stat_punkter_punkter funktion - for 2 og 3 var
    
    Niveaukurve funktioner
    
    Ny grad funktion
    
    retrieve_name and num_of_vars til generelt brug i mat funktioner

--- testerfunc.py
+++ testerfunc.py
@@ -1,7 +1,21 @@
 from sympy import *
 from sympy.abc import x,y,z,v,u,w
 from IPython.display import display, Math, Latex
+import inspect
 init_printing()
+
+
+# non math related
+def retrieve_name(var):
+    callers_local_vars = inspect.currentframe().f_back.f_back.f_locals.items()
+    return [var_name for var_name, var_val in callers_local_vars if var_val is var]
+
+def num_of_vars(f):
+    vars = list(f.free_symbols)
+    return len(vars)
+
+
+
 
 
 def flux(V, r, firstint, secondint):
@@ -23,11 +37,8 @@ def jacobi_kurve(r, var):
     else:
         return simplify(sqrt(rdiff[0]**2+rdiff[1]**2+rdiff[2]**2))
 
-
-def flowkurve_w_bb(V, tid, rt):
-    x = Function("x")
-    y = Function("y")
-    z = Function("z")
+#fiks, så kan bruges uden med x frem for x(t)
+def flowkurve_w_bb_rum(V, tid, rt):
     t = symbols('t')
     eq1 = Eq(diff(x(t),t), V[0])
     eq2 = Eq(diff(y(t),t), V[1])
@@ -35,17 +46,113 @@ def flowkurve_w_bb(V, tid, rt):
     sol = dsolve([eq1, eq2, eq3],ics={x(tid) : rt[0], y(tid) : rt[1], z(tid) : rt[2]})    
     return Matrix([sol[0].rhs, sol[1].rhs, sol[2].rhs])
 
+def flowkurve_w_bb_plan(V, tid, rt):
+    t = symbols('t')
+    eq1 = Eq(diff(x(t),t), V[0])
+    eq2 = Eq(diff(y(t),t), V[1])
+    sol = dsolve([eq1, eq2],ics={x(tid) : rt[0], y(tid) : rt[1]})    
+    return Matrix([sol[0].rhs, sol[1].rhs])
+
 def drej_om_z_akse(r):
     Q = Matrix([[cos(w), sin(w), 0], [-sin(w), cos(w), 0], [0,0,1]])
     return Q*r
 
-def grad(f):
-    return Matrix([diff(f,x), diff(f,y)])
 
-def stat_punkter(grad):
+#Tager en funktion som input. Kan kun bruges til funktioner af x,y og z for nu.
+def grad(f):
+    number_of_vars = num_of_vars(f)
+    var_list = [x, y, z]
+    sol = Matrix([])
+    for i in range(number_of_vars):
+        sol = sol.row_insert(sol.shape[0], Matrix([diff(f, var_list[i])]))
+    return sol
+
+#Simpel niveaukurve. Tager en funktion og en konstant, som indput. 
+def niveaukurve(f, c, text=True):
+    if text == False:
+        return simplify(Eq(f,c))
+    string1 = r'''Niveaukurven \: for \: {%s} \: svarende \: til \: niveauet \: {%s} \: er \: givet \: ved: {%s}''' % (retrieve_name(f)[0], latex(c), latex(simplify(Eq(f,c))))
+    display(Math(string1))
+
+    return simplify(Eq(f,c))
+
+#Funktionen her forsøger at give et udtryk for niveaukurven, som funktion af både x og y. 
+#Tager en funktion og konstant som input.
+def niveaukurve_undersøgelse(f, c):
+    try:
+        eq1 = simplify(Eq(solve(niveaukurve(f, c, text=False),y)[0], y))
+        string1 = r'''Niveaukurven \: for \: {%s} \: svarende \: til \: niveauet \: {%s} \: som \: funktion \: af \: x \: er \: givet \: ved: {%s}''' % (retrieve_name(f)[0], latex(c), latex(eq1))
+        display(Math(string1))
+    except:
+        print('NaN')
+    try:
+        eq2 = simplify(Eq(solve(niveaukurve(f, c, text=False),x)[0], x))
+        string2 = r'''Niveaukurven \: for \: {%s} \: svarende \: til \: niveauet \: {%s} \: som \: funktion \: af \: y \: er \: givet \: ved: {%s}''' % (retrieve_name(f)[0], latex(c), latex(eq2))
+        display(Math(string2))
+    except:
+        print('Nan')
+
+
+
+#Tager en gradient på matrix form. funcvar er funktionens navn ex: funcvar = 'f'
+def stat_punkter_2_var(grad, funcvar, text=True):
     eq1 = Eq(grad[0], 0)
     eq2 = Eq(grad[1], 0)
+    if text == False:
+        return solve([eq1, eq2], [x,y])
+    string1 = r'''De \: stationære \: punkter \: findes \: ved at \: løse \: følgende \: ligningssystem:'''
+    string2 = r'''\frac{\partial}{\partial x}{%s}=0''' % (latex(funcvar))
+    string3 = r'''\frac{\partial}{\partial y}{%s}=0''' % (latex(funcvar))
+    string4 = r'''Herved \: opstilles \: ligningssystemet:'''
+    string5 = r'''{%s}=0''' % (latex(grad[0]))
+    string6 = r'''{%s}=0''' % (latex(grad[1]))
+    string7 = r'''Løsningerne \: udregnes \: til: {%s}''' % (latex(solve([eq1, eq2], [x,y])))
+    string8 = r'''Følgende \: er \: altså \: stationære \: punkter \: for \: funktionen, {%s}:''' % (latex(funcvar))
+    display(Math(string1))
+    display(Math(string2))
+    display(Math(string3))
+    display(Math(string4))
+    display(Math(string5))
+    display(Math(string6))
+    display(Math(string7))
+    display(Math(string8))
+    for i in range(len(solve([eq1, eq2], [x,y]))):
+        stringx = r'''Punkt \: {%s} = {%s}''' % (latex(i), latex((solve([eq1, eq2], [x,y])[i])))
+        display(Math(stringx))
     return solve([eq1, eq2], [x,y])
+
+
+#Tager en gradient på matrix form. funcvar er funktionens navn ex: funcvar = 'f'
+def stat_punkter_3_var(grad, funcvar, text=True):
+    eq1 = Eq(grad[0], 0)
+    eq2 = Eq(grad[1], 0)
+    eq3 = Eq(grad[2], 0)
+    if text == False:
+        return solve([eq1, eq2, eq3], [x,y,z])
+    string1 = r'''De \: stationære \: punkter \: findes \: ved at \: løse \: følgende \: ligningssystem:'''
+    string2 = r'''\frac{\partial}{\partial x}{%s}=0''' % (latex(funcvar))
+    string3 = r'''\frac{\partial}{\partial y}{%s}=0''' % (latex(funcvar))
+    string4 = r'''\frac{\partial}{\partial z}{%s}=0''' % (latex(funcvar))
+    string5 = r'''Herved \: opstilles \: ligningssystemet:'''
+    string6 = r'''{%s}=0''' % (latex(grad[0]))
+    string7 = r'''{%s}=0''' % (latex(grad[1]))
+    string8 = r'''{%s}=0''' % (latex(grad[2]))
+    string9 = r'''Løsningerne \: udregnes \: til: {%s}''' % (latex(solve([eq1, eq2, eq3], [x,y,z])))
+    string10 = r'''Følgende \: er \: altså \: stationære \: punkter \: for \: funktionen, {%s}:''' % (latex(funcvar))
+    display(Math(string1))
+    display(Math(string2))
+    display(Math(string3))
+    display(Math(string4))
+    display(Math(string5))
+    display(Math(string6))
+    display(Math(string7))
+    display(Math(string8))
+    display(Math(string9))
+    display(Math(string10))
+    for i in range(len(solve([eq1, eq2, eq3], [x,y,z]))):
+        stringx = r'''Punkt \: {%s} = {%s}''' % (latex(i), latex((solve([eq1, eq2, eq3], [x,y,z])[i])))
+        display(Math(stringx))
+    return solve([eq1, eq2, eq3], [x,y,z])
 
 def partil_afledede(f, var, i):
     return diff(f, var, i)
@@ -93,8 +200,10 @@ def approksimerende_andengrads_pol(f, udviklingspunkt):
     return first + second + third + fourth
 
 
-def div(V):
-    return diff(V[0], x) + diff(V[1], y) + diff(V[2], z)
+def div(V, vars):
+    dV = Matrix([diff(V[i], vars[i]) for i in range(len(V))])
+    return sum(dV[i] for i in range(len(V)))
+
 
 
 def jacobi_rum(r):
@@ -144,3 +253,18 @@ def jacobi_flade(r):
 def tangentielle_kurveintegral(V, r, var, tstart, tslut):
     vru = V.subs(x, r[0]).subs(y, r[1]).subs(z, r[2])
     return integrate(vru*jacobi_kurve(r, var), (var, tstart, tslut))
+
+def rot(V):
+    return Matrix([(diff(V[2], y)-diff(V[1], z)), (diff(V[0], z)-diff(V[2], x)), (diff(V[1], x)-diff(V[0], y))])
+
+def tjek_v_gradfelt(V):
+    if rot(V).norm() == 0:
+        return True
+    else:
+        return False
+    
+def stam_til_vektorfelt(V):
+    Vuxuyuz = Matrix([u*x,u*y,u*z])
+    delres = Matrix([Vuxuyuz[0].subs(x, V[0]), Vuxuyuz[1].subs(y, V[1]), Vuxuyuz[2].subs(z, V[2])])
+    sol = Matrix([x,y,z]).dot(Matrix([integrate(delres[0], (u,0,1)), integrate(delres[1], (u,0,1)), integrate(delres[2], (u,0,1))]))
+    return sol
